1203e32a661d5c6a105a4e4e470cd0f7a3c460c8
aniket-salunkhe <aniket.salunkhe@wissen.com>
2021-07-07 10:57:07 +0530

    Adding the Maven Project

---
+++ src/test/java/com/FirstMavenProgram/AppTest.java
@@ -0,0 +1,20 @@
+package com.FirstMavenProgram;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/com/FirstMavenProgram/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.SecondMavenProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	Calculator c1 = new Calculator();
+
+    @Test
+    public void testAdd()
+    { 
+		
+        assertEquals(30,c1.add(10,20));
+    }
+
+
+@Test
+    public void testSub()
+    {
+        assertEquals(-10,c1.sub(10,20));
+    }
+	
+	@Test
+    public void testMultiply()
+    {
+        assertEquals(200,c1.multiply(10,20));
+    }
+	
+	
+	@Test
+    public void testDivision()
+    {
+        assertEquals(0,c1.division(10,20));
+    }
+
+}
